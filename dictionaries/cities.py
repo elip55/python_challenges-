a2b44230ce40f65e2a77c8ca93ef9623042e4f60
Eli Pacheco <eli.pacheco55@gmail.com>
2021-05-01 11:59:59 -0600

    City dictionary

---
+++ dictionaries/cities.py
@@ -0,0 +1,9 @@
+# create a dictionary called 'cities'
+# use three cities as keys 
+# Include country, population, and one fact
+
+cities = [{'Albuquerque': 'USA', 'population':559374, 'cool fact':'Largest city of New Mexico '},
+         {'Denver': 'USA', 'population':705576, 'cool fact':'Stole green chile from New Mexico'},
+         {'Rome': 'Italy', 'population':2873000, 'cool face': 'Home of the pope'}]
+for i in cities:
+    print(i)
