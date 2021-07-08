7dedf2e67d77e5464bded461b8117d7be1dc752d
Eli Pacheco <elipacheco@elis-imac.lan>
2021-07-08 09:46:50 -0600

    more user friendly

--- interview_q/hourly_to_yearly.py
+++ interview_q/hourly_to_yearly.py
@@ -4,7 +4,14 @@ def calculations(pay,hours):
     yearly_salary = (pay*hours)*52
     print(yearly_salary)
 
-
-pay = float(input("How much do you get paid an hour?"))
-hours = float(input("How many hours do you work per week?"))
+try:
+    pay = float(input("How much do you get paid an hour? "))
+except:
+    print("ERROR:  PLEASE ENTER THE AMOUNT IN DOLLARS AND CENTS!\nPLEASE RUN THE PROGRAM AGAIN!")
+    exit()
+try:
+    hours = float(input("How many hours do you work per week? "))
+except:
+    print("ERROR:  PLEASE ENTER THE EXACT AMOUNT OF HOURS.\nPLEASE RUN THE PROGRAM AGAIN")
+    exit()
 calculations(pay,hours)
