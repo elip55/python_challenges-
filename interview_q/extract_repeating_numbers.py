3d036ed9ebf6c2cf458a0a96f938cc2dd7ca47a2
Eli Pacheco <elipacheco@Elis-MacBook-Air.local>
2023-06-11 18:40:28 -0600

    added a print function for testing

--- interview_q/extract_repeating_numbers.py
+++ interview_q/extract_repeating_numbers.py
@@ -2,6 +2,8 @@
 # 1: Extract repeating integers from an arr
 # 2: Write a .txt document displaying those numbers
 
+from make_random_list import make_a_random_list # Imported a function I made to generate a random list
+
 """NEEDS OPTIMIZATION"""
 def extract_numbers(arr):
     # first initialize the lists
@@ -30,4 +32,22 @@ def build_txt(arr):
     with open('repeating number.txt', 'w') as writer:
         writer.write(buildit)
 
-        
+"""TO TEST THE CODE PRINT INSTEAD OF RETURN"""
+def print_extracted_numbers(arr):
+    # first initialize the lists
+    l1 = []
+    l2 = []
+    # bubble up the list so that the numbers are in ascending order
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    # then compare each element to extract the repeating numbers
+    for k in range(len(arr)-1):
+        if arr[k] == arr[k+1]:
+            l1.append(arr[k])
+    # finally, append the final list without any repeated numbers
+    for l in l1:
+        if l not in l2:
+            l2.append(l)
+    print(l2)
