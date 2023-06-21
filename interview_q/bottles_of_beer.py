dbb93a120da2a9730f9c5a49c0395022bfba0438
Eli Pacheco <elipacheco@elis-air.lan>
2023-06-20 18:00:47 -0600

    added bottles of beer song

---
+++ interview_q/bottles_of_beer.py
@@ -0,0 +1,12 @@
+
+#  write code that sings '100 bottles of beer on the wall'
+
+
+bottles_of_beer = 99
+
+while bottles_of_beer != 0:
+    print(f'{bottles_of_beer} bottles of beer on the wall!')
+    print(f'{bottles_of_beer} bottles of beeeer!')
+    print(f'Take one down,\nPass it around')
+    bottles_of_beer -= 1
+    print(f'{bottles_of_beer} bottles of beer on the wall!\n')
